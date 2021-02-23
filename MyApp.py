61481171f33126e32bb28e2256546bf94290f4db
kundan <kundanbhardwaj145@gmail.com>
2021-02-23 19:23:32 +0530

    modified again

--- MyApp.py
+++ MyApp.py
@@ -8,14 +8,13 @@ from kivy.uix.videoplayer import VideoPlayer
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import AsyncImage
 from kivy.uix.scrollview  import ScrollView
-from kivy.uix.screenmanager import Screen,ScreenManager,NoTransition, SwapTransition
+from kivy.uix.screenmanager import Screen,ScreenManager,NoTransition, FadeTransition, RiseInTransition
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.toolbar import MDToolbar
 from kivymd.uix.list import MDList , OneLineAvatarListItem
 from kivymd.uix.navigationdrawer import MDNavigationDrawer, NavigationLayout
 from kivymd.app import MDApp
 from urls import*
-sc=ScreenManager()
 class MenuScreen(Screen):		
 	def set_sc(self):
 	    MDApp.get_running_app().root.current = "profile"
@@ -58,9 +57,14 @@ class profilescreennine(Screen):
 class subscreenone(Screen):  	
     def ch_sc(self):
         MDApp.get_running_app().root.current = "menu"
-        MDApp.get_running_app().root.transition.direction="down"
+        MDApp.get_running_app().root.transition.direction="down"        
 class MainApp (MDApp):
 	def build(self):
-		frame=sc
-		return frame
+		l=['Red', 'Purple', 'DeepPurple', 'Indigo', 'Blue', 'LightBlue', 'Cyan', 'Teal', 'Green', 'LightGreen', 'Orange', 'DeepOrange',]
+		self.theme_cls.primary_palette = random.choice(l)		
+		sc=ScreenManager(transition=FadeTransition(duration=2))
+		sc.add_widget(MenuScreen(name="menu"))
+		sc.add_widget(profilescreentwo(name="profile2"))
+		sc.add_widget(profilescreenthree(name="profile3"))				
+		return sc
 MainApp().run()
