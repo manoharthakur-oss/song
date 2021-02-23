e72d7ec435bf749b8d8615d5f9a168d838a98eda
kundan <kundanbhardwaj145@gmail.com>
2021-02-23 08:07:11 +0530

    finaly i have adjusted it

--- MyApp.py
+++ MyApp.py
@@ -8,29 +8,60 @@ from kivy.uix.videoplayer import VideoPlayer
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import AsyncImage
 from kivy.uix.scrollview  import ScrollView
-from kivy.uix.screenmanager import Screen,ScreenManager,NoTransition
+from kivy.uix.screenmanager import Screen,ScreenManager,NoTransition, SwapTransition
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.toolbar import MDToolbar
 from kivymd.uix.list import MDList , OneLineAvatarListItem
 from kivymd.uix.navigationdrawer import MDNavigationDrawer, NavigationLayout
 from kivymd.app import MDApp
 from urls import *
-class MenuScreen(Screen):
+class ScrManager (ScreenManager):
+	transition=SwapTransition()
+class MenuScreen(Screen):		
 	def set_sc(self):
 	    MDApp.get_running_app().root.current = "profile"
 	    MDApp.get_running_app().root.transition.direction="up"
 	def set_screen(self):
 		MDApp.get_running_app().root.current = "profile1"
 		MDApp.get_running_app().root.transition = NoTransition(duration=0.001)
+class profilescreentwo(Screen):
+    def ch_sc(self):
+        MDApp.get_running_app().root.current = "menu"
+        MDApp.get_running_app().root.transition.direction="down"   
+class profilescreenthree(Screen):
+    def ch_sc(self):
+        MDApp.get_running_app().root.current = "menu"
+        MDApp.get_running_app().root.transition.direction="down"
+class profilescreenfour(Screen):
+    def ch_sc(self):
+        MDApp.get_running_app().root.current = "menu"
+        MDApp.get_running_app().root.transition.direction="down"
+class profilescreenfive(Screen):
+    def ch_sc(self):
+        MDApp.get_running_app().root.current = "menu"
+        MDApp.get_running_app().root.transition.direction="down"  	
+class profilescreensix(Screen):
+    def ch_sc(self):
+        MDApp.get_running_app().root.current = "menu"
+        MDApp.get_running_app().root.transition.direction="down"
+class profilescreenseven(Screen):
+    def ch_sc(self):
+        MDApp.get_running_app().root.current = "menu"
+        MDApp.get_running_app().root.transition.direction="down"
+class profilescreeneight(Screen):
+    def ch_sc(self):
+        MDApp.get_running_app().root.current = "menu"
+        MDApp.get_running_app().root.transition.direction="down"
+class profilescreennine(Screen):
+    def ch_sc(self):
+        MDApp.get_running_app().root.current = "menu"
+        MDApp.get_running_app().root.transition.direction="down"
+class subscreenone(Screen):  	
+    def ch_sc(self):
+        MDApp.get_running_app().root.current = "menu"
+        MDApp.get_running_app().root.transition.direction="down"
 class MainApp (MDApp):
-	
 	def build(self):
-		frame = Builder.load_file("main.kv")
+		frame= ScrManager()
 		return frame
-
-
-
-
-
-
 MainApp().run()
