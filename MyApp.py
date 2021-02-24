612f5fc1d4dcc938ea709fefea9f5909897a8272
manohar thakur redmi <thakurmanohar@gmail.com>
2021-02-24 22:05:51 +0530

    modified navigation drawer

--- MyApp.py
+++ MyApp.py
@@ -11,9 +11,6 @@ from kivy.uix.scrollview  import ScrollView
 from kivy.uix.screenmanager import Screen,ScreenManager,NoTransition, FadeTransition, RiseInTransition
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.list import OneLineAvatarListItem , TwoLineAvatarListItem, ThreeLineAvatarListItem, OneLineIconListItem, TwoLineIconListItem ,ThreeLineIconListItem
-from kivymd.uix.list import OneLineAvatarIconListItem
-from kivymd.uix.list import TwoLineAvatarIconListItem
-from kivymd.uix.list import ThreeLineAvatarIconListItem
 from kivymd.uix.toolbar import MDToolbar
 from kivymd.uix.list import MDList , OneLineAvatarListItem
 from kivymd.uix.navigationdrawer import MDNavigationDrawer, NavigationLayout
@@ -25,7 +22,9 @@ from urls import*
 
 def change_it():
 	MDApp.get_running_app().root.current = "menu"
-	MDApp.get_running_app().root.transition.direction="down"
+	#MDApp.get_running_app().root.transition = FadeTransition()
+	MDApp.get_running_app().root.transition.direction="up"
+	MDApp.get_running_app().root.transition.duration=10
 
 # SCREENS
 "================================================================================================"
@@ -120,24 +119,7 @@ class Vbox (MDBoxLayout):
 
 # LIST ITEMS
 "================================================================================================"
-class olali (OneLineAvatarListItem):
-	pass 
-class tlali (TwoLineAvatarListItem):
-	pass 
-class thlali (ThreeLineAvatarListItem):
-	pass 
-class olili (OneLineIconListItem):
-	pass 
-class tlili (TwoLineIconListItem):
-	pass 
-class thlili (ThreeLineIconListItem):
-	pass
-class olaili():
-	pass
-class tlaili():
-	pass
-class thlaili():
-	pass
+
 
 
 class MainApp (MDApp):
@@ -147,7 +129,7 @@ class MainApp (MDApp):
 	def build(self):
 		l=['Red', 'Purple', 'DeepPurple', 'Indigo', 'Blue', 'LightBlue', 'Cyan', 'Teal', 'Green', 'LightGreen', 'Orange', 'DeepOrange',]
 		self.theme_cls.primary_palette = random.choice(l)		
-		sc=ScreenManager(transition=FadeTransition(duration=0.01))
+		sc=ScreenManager()
 		sc.add_widget(MenuScreen(name="menu"))
 		sc.add_widget(profilescreentwo(name="profile2"))
 		sc.add_widget(profilescreenthree(name="profile3"))				
