2ffd2bb5600fef8916a870daed2a3c8bf87961e8
manohar thakur redmi <thakurmanohar@gmail.com>
2021-02-24 22:41:42 +0530

    added insta and git hub links *

--- urls.py
+++ urls.py
@@ -5,4 +5,4 @@ kivy = "https://kivy.org"
 
 class we :
 	instagram = "https://www.instagram.com/manoharlal_thakur/"
-	GitHub = "https://github.com/manoharthakur351"
+	github = "https://github.com/manoharthakur351"
