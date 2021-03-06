3b20d4a1097f8ba7a1d9f83c6b22959c437615d4
manohar thakur redmi <thakurmanohar@gmail.com>
2021-03-06 21:08:23 +0530

    added language related images

--- MyApp.py
+++ MyApp.py
@@ -39,7 +39,6 @@ def load_sub4 ():
 
 def load ():
 	Builder.load_file(os.path.join("main.kv"))
-	#Builder.load_file("main.kv")
 	Builder.load_file("layout.kv")
 	Builder.load_file("screens/menu.kv")
 	Builder.load_file("screens/screen1.kv")
@@ -65,76 +64,6 @@ class Manager(ScreenManager):
 
 # SCREEN(S)
 #________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
-
-class MenuScreen(Screen):
-	"""
-	
-	"""
-	
-	pass
-
-class profilescreenone(Screen):
-	"""
-	
-	"""
-	def ch_sc (self):
-		switch()
-	
-
-class profilescreentwo(Screen):
-	"""
-	
-	"""
-	def ch_sc (self):
-		switch()
-
-class profilescreenthree(Screen):
-	"""
-	
-	"""
-	def ch_sc (self):
-		switch()
-
-class profilescreenfour(Screen):
-	"""
-	
-	"""
-	def ch_sc (self):
-		switch()
-
-class profilescreenfive(Screen):
-	"""
-	
-	"""
-	def ch_sc (self):
-		switch()
-
-class profilescreensix(Screen):
-	"""
-	
-	"""
-	def ch_sc (self):
-		switch()
-
-class profilescreenseven(Screen):
-	"""
-	
-	"""
-	def ch_sc (self):
-		switch()
-
-class profilescreeneight(Screen):
-	"""
-	
-	"""
-	def ch_sc (self):
-		switch()
-
-def switch ():
-	MDApp.get_running_app().root.current = "menu"
-	MDApp.get_running_app().root.transition = NoTransition()
-	MDApp.get_running_app().root.transition.direction="right"
-	#MDApp.get_running_app().root.transition.duration=0.1
 
 
 
--- screen.py
+++ screen.py
@@ -2,3 +2,74 @@ from kivy.uix.screenmanager import Screen,ScreenManager,NoTransition, FadeTransi
 
 class subscreenone (Screen):
 	pass
+
+class MenuScreen(Screen):
+	"""
+	
+	"""
+	
+	pass
+
+class profilescreenone(Screen):
+	"""
+	
+	"""
+	def ch_sc (self):
+		switch()
+	
+
+class profilescreentwo(Screen):
+	"""
+	
+	"""
+	def ch_sc (self):
+		switch()
+
+class profilescreenthree(Screen):
+	"""
+	
+	"""
+	def ch_sc (self):
+		switch()
+
+class profilescreenfour(Screen):
+	"""
+	
+	"""
+	def ch_sc (self):
+		switch()
+
+class profilescreenfive(Screen):
+	"""
+	
+	"""
+	def ch_sc (self):
+		switch()
+
+class profilescreensix(Screen):
+	"""
+	
+	"""
+	def ch_sc (self):
+		switch()
+
+class profilescreenseven(Screen):
+	"""
+	
+	"""
+	def ch_sc (self):
+		switch()
+
+class profilescreeneight(Screen):
+	"""
+	
+	"""
+	def ch_sc (self):
+		switch()
+
+def switch (screen_name):
+	MDApp.get_running_app().root.current = screen_name
+	MDApp.get_running_app().root.transition = NoTransition()
+	MDApp.get_running_app().root.transition.direction="right"
+	#MDApp.get_running_app().root.transition.duration=0.1
+
